bd4067fbce72987b0b9677341eea7e99e201d01e
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-07-30 13:07:43 +0530

    Shoppin-Cart CRUD

---
+++ Shopping-cart/build/precache-manifest.3c99f7eb9a628b6a098389fd7ae162f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae2af5e32f3f2d59c4c04015b005edd",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "b7d7fc4d79e7147d1e1b",
+    "url": "/shopping-cart/static/css/main.78383970.chunk.css"
+  },
+  {
+    "revision": "27d1eeba72939cea416d",
+    "url": "/shopping-cart/static/js/2.945c09ac.chunk.js"
+  },
+  {
+    "revision": "a27a10e52e3d9ffc0e4a7c94b342b1f1",
+    "url": "/shopping-cart/static/js/2.945c09ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d7fc4d79e7147d1e1b",
+    "url": "/shopping-cart/static/js/main.efb053d7.chunk.js"
+  },
+  {
+    "revision": "31b14a9c0b037069b431",
+    "url": "/shopping-cart/static/js/runtime-main.22eec299.js"
+  }
+]);
