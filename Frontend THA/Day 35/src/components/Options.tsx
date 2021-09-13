b7f5ba87de9f27b05748a31d43f00494f0b6f1ca
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-09-13 10:11:38 +0530

    Add files via upload

---
+++ Frontend THA/Day 35/src/components/Options.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Options = () => {
+  return (
+    <div className="options">
+      <div>Switch Account</div>
+      <div>Follow More</div>
+    </div>
+  );
+};
+
+export default Options;
